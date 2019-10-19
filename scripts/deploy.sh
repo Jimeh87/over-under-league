d1b11a08f6fd2f7fd44988daaada7d6f7e9f7c9c
Jim Rennie <JLRennie@gmail.com>
2019-10-19 14:57:32 -0600

    strip directories from tar jar and extract tar to deployment folder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,6 @@ eval "$(ssh-agent -s)"
 chmod 600 ./prod_deploy_key
 echo -e "Host $PROD_SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./prod_deploy_key
-tar -czvf over-under-league.tar.gz "${TRAVIS_BUILD_DIR}/target/over-under-league.jar"
+tar -czvf over-under-league.tar.gz -C "${TRAVIS_BUILD_DIR}/target" over-under-league.jar
 scp -i ./prod_deploy_key over-under-league.tar.gz "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}:"
-ssh -i ./prod_deploy_key "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}" pwd
+ssh -i ./prod_deploy_key "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}" 'tar -xvf over-under-league.tar.gz -C /srv/over-under-league/'
