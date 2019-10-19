282711cd445111586b3ed42dcc56950e884c0a2a
Jim Rennie <JLRennie@gmail.com>
2019-10-19 15:03:05 -0600

    add better echos for deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,6 @@
 
 set -ev
 
-#scp ${TRAVIS_BUILD_DIR}/over-under-league.jar
 echo "Starting deploy..."
 
 openssl aes-256-cbc -K $encrypted_eeb17145cacf_key -iv $encrypted_eeb17145cacf_iv -in prod_deploy_key.enc -out prod_deploy_key -d
@@ -13,3 +12,5 @@ ssh-add ./prod_deploy_key
 tar -czvf over-under-league.tar.gz -C "${TRAVIS_BUILD_DIR}/target" over-under-league.jar
 scp -i ./prod_deploy_key over-under-league.tar.gz "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}:"
 ssh -i ./prod_deploy_key "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}" 'tar -xvf over-under-league.tar.gz -C /srv/over-under-league/'
+
+echo "Deploy complete!"
