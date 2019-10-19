2991b4a2d68a0c7cecdc16139badd2ee01e24704
Jim Rennie <JLRennie@gmail.com>
2019-10-19 11:44:33 -0600

    travis prod deploy stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set -ev
+
 #scp ${TRAVIS_BUILD_DIR}/over-under-league.jar
 echo "Starting deploy..."
 
-for entry in "${TRAVIS_BUILD_DIR}"/*
-do
-  echo "$entry"
-done
+openssl aes-256-cbc -K $encrypted_eeb17145cacf_key -iv $encrypted_eeb17145cacf_iv -in prod_deploy_key.enc -out prod_deploy_key -d
+eval "$(ssh-agent -s)"
+chmod 600 ./prod_deploy_key
+echo -e "Host $PROD_SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ./prod_deploy_key
+scp -i ./prod_deploy_key "${TRAVIS_BUILD_DIR}/over-under-league.jar" "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}"
+ssh -i ./prod_deploy_key "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}" pwd
+
+tar -czvf over-under-league.tar.gz "${TRAVIS_BUILD_DIR}/target/over-under-league.jar"
