346f2452d2fc715019dbd21824e4ab438e7e46f2
Jim Rennie <JLRennie@gmail.com>
2022-10-18 20:00:00 -0600

    disabling strict host checking

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ set -ev
 echo "Starting deploy..."
 
 tar -czvf over-under-league.tar.gz -C ./target over-under-league.jar
-scp over-under-league.tar.gz "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}:"
-ssh "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}" 'tar -xvf over-under-league.tar.gz -C /srv/over-under-league/ && chmod 755 /srv/over-under-league/over-under-league.jar && sudo /bin/systemctl restart over-under-league'
+scp -o StrictHostKeyChecking=no over-under-league.tar.gz "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}:"
+ssh -o StrictHostKeyChecking=no "${PROD_USER}@${PROD_SERVER_IP_ADDRESS}" 'tar -xvf over-under-league.tar.gz -C /srv/over-under-league/ && chmod 755 /srv/over-under-league/over-under-league.jar && sudo /bin/systemctl restart over-under-league'
 
 echo "Deploy complete!"
