7c8175facb4625c59fe21d1ae19bb12b29b60884
Jim Rennie <JLRennie@gmail.com>
2024-10-31 21:41:06 -0600

    upgrade angular 18 and latest sb2

--- src/main/angular/app/app.component.ts
+++ src/main/angular/app/app.component.ts
@@ -1,19 +1,13 @@
-import {Component} from '@angular/core';
-import {MatIconRegistry} from "@angular/material/icon";
-import {DomSanitizer} from "@angular/platform-browser";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon('sports-basketball', sanitizer.bypassSecurityTrustResourceUrl('assets/sports_basketball-24px.svg'));
-    iconRegistry.addSvgIcon('arrow-down', sanitizer.bypassSecurityTrustResourceUrl('assets/keyboard_arrow_down-24px.svg'));
-    iconRegistry.addSvgIcon('arrow-up', sanitizer.bypassSecurityTrustResourceUrl('assets/keyboard_arrow_up-24px.svg'));
-    iconRegistry.addSvgIcon('github', sanitizer.bypassSecurityTrustResourceUrl('assets/github.svg'));
-  }
-
+  title = 'overunderleague';
 }
